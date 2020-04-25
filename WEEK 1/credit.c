80bf36578d7893d4534bf89edb38b622bc8c8a0a
Vivek <48525663+vivek-sheta@users.noreply.github.com>
2020-04-25 12:35:00 +0530

    add comment

--- WEEK 1/credit.c
+++ WEEK 1/credit.c
@@ -8,44 +8,47 @@ int main(void){
     long long int card_number;
     do
     {
-        card_number=get_long("Card Number : ");
-    }while(card_number<=0);
+        card_number=get_long("Card Number : ");  //check card number is positive and >0
+    }
+    while(card_number<=0);
 
-    if(checksum(card_number))
-    {
-        printf("%s\n",check_type(card_number));
-    }else printf("INVALID\n");
+    if (checksum(card_number))  //check card number validity using Luhn’s Algorithm
+        printf("%s\n",check_type(card_number));  //check type of card like. master,visa,AMEX
+    else 
+        printf("INVALID\n");
 }
 
-bool checksum(long long int number){
+//Luhn’s Algorithm CHECKSUM
+bool checksum(long long int number)
+{
     int length=0,hold,even_mul;
     long long int temp;
     bool odd=1;
     int sum_odd=0,sum_even=0;
     temp=number;
-    while(temp != 0)
+    while(temp != 0)  //find length of card number
     {
         temp /= 10;
-        length++;
+        length++; 
     }
 
-    if(length <=12||length>=17)
+    if(length <=12||length>=17)  //reject card number based on length
     {
         return 0;
     }
     else
     {
-         while(number != 0)
+         while(number != 0)  
          {
-            hold = number % 10;
-            if(odd)
+            hold = number % 10;  //get last digit of card number
+            if(odd) //check is digit odd or even? and do operation
             {
-                sum_odd += hold;
+                sum_odd += hold;  //simply add odd number 
                 odd = 0;
             }
             else
             {
-                even_mul = hold * 2;
+                even_mul = hold * 2;  //even number * 2 and seperate digit and add eg. 6*2=12 (1+2) 
                 while (even_mul != 0)
                 {
                     sum_even += even_mul % 10;
@@ -53,16 +56,16 @@ bool checksum(long long int number){
                 }
                 odd = 1;
             }
-         number /= 10;
+         number /= 10; //delete the last digit which get before
         }
     }
-    if((sum_odd + sum_even)%10)
+    if((sum_odd + sum_even)%10) //if mod of (odd+even = 0) return true else false
         return 0;
     else
         return 1;
 }
 
-
+//Check type of card 
 string check_type(long long int number)
 {
     int length=0;
