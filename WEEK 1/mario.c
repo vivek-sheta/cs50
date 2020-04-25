d751274ed82d21a2c58868f87b5fa6cac4d69ae5
Vivek <48525663+vivek-sheta@users.noreply.github.com>
2020-04-25 12:36:14 +0530

    add comment

--- WEEK 1/mario.c
+++ WEEK 1/mario.c
@@ -14,16 +14,16 @@ int main(void)
   for( ; i<=height ; ++i, space=0,temp=0,count-=1)
   {
       while(space!=count)
-        { printf(" "); space++; } //print spaces as per needed 
+        { printf(" "); space++; } // print spaces as per needed 
       while(temp!=i)
-        { printf("#"); temp++; }  //print left side pyramid 
+        { printf("#"); temp++; }  // print left side pyramid 
     
-      printf("  ");  //print two spaces in between pyramid
+      printf("  ");  // print two spaces in between pyramid
     
-      temp=0;  //set temp to 0 for print right side pyramid
+      temp=0;  // set temp to 0 for print right side pyramid
     
       while(temp!=i)
-        {  printf("#");  temp++; } //print right side pyramid
-     printf("\n"); //new line 
+        {  printf("#");  temp++; } // print right side pyramid
+     printf("\n"); // new line 
   }
 }
